2e66033f1a6537145072edecce00eef317b4388c
dona1214 <76546326+dona1214@users.noreply.github.com>
2023-08-04 22:58:01 +0900

    Fix setting error and build error.

---
+++ app/src/main/java/com/example/mydog/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mydog;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.mydog.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    public ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+         //       R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+          //      .build();
+        //NavController navController = Navigation.findNavController(this, R.id.navigation_notifications);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupWithNavController(navView, navController);
+        setContentView(R.layout.activity_login);
+    }
+
+}
